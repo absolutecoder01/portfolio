fe632f5211aa1c7d27e57e773be5c6794afa55e4
absolutecoder01 <absolute.coder01@gmail.com>
2025-04-12 17:16:43 +0200

    Maybe final changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -31,7 +31,44 @@ export function Projects() {
             tech={['React', 'TypeScript', 'Tailwindcss']}
             image="public/stars-for-car-ceiling-v0-d4p6bwerbf7e1.webp"
           />
+          <ProjectCard 
+            title="Streamsoft Services Overview"
+            description="A web application showcasing the services of Streamsoft. Developed during practice at Streamsoft."
+            tech={['React', 'TypeScript', 'Tailwindcss']}
+            image="public/streamsoft_services.png"
+          />
+          <ProjectCard 
+            title="Modern GUI Notepad"
+            description="Simple notepad with 'modern' dark theme."
+            tech={['Java', 'Java Swing', 'Flatlaf']}
+            image="public/notepad_showcase.png"
+          />
         </div>
+        <div className="container mx-auto px-6">
+        <motion.h2 
+          className="text-3xl font-bold mb-12 text-white"
+          initial={{ x: -20 }}
+          whileInView={{ x: 0 }}
+          viewport={{ once: true }}
+        >
+          <br/>
+          Current projects
+        </motion.h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectCard 
+            title="StudyFlow"
+            description="The app helps users plan their studies, track their progress, and organize their materials. Perfect for students, pupils, or independent learners."
+            tech={['React', 'Tailwindcss', 'Java', 'SpringBoot']}
+            image="public/under_construction.png"
+          />
+          <ProjectCard 
+            title="e-Senior"
+            description="e-Senior is a web application that allows teaching older people how to use modern technologies. Implemented as a social project within the ZwolnieniZTeorii program."
+            tech={['React', 'TypeScript', 'Tailwindcss']}
+            image="public/e-senior.png"
+          />
+        </div>
+      </div>
       </div>
     </motion.section>
   );
